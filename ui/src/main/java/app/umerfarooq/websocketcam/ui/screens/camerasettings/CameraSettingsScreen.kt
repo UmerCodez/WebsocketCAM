831ce3bb41fb21f5af958b9b125b439a751aa5b2
UmerCodez <umerfarooq.phone@gmail.com>
2026-05-22 22:14:14 +0500

    fix: Enable vertical scrolling in CameraSettingsScreen
    Add `verticalScroll` to the main Column in `CameraSettingsScreen` to ensure content is accessible on smaller screens or when keyboard is visible.

--- ui/src/main/java/app/umerfarooq/websocketcam/ui/screens/camerasettings/CameraSettingsScreen.kt
+++ ui/src/main/java/app/umerfarooq/websocketcam/ui/screens/camerasettings/CameraSettingsScreen.kt
@@ -23,7 +23,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -72,6 +74,7 @@ fun CameraSettingsScreen(
     Surface {
         Column(
             modifier = Modifier
+                .verticalScroll(rememberScrollState())
                 .fillMaxSize()
                 .padding(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
